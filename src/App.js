f4f3bc1f69c17eba72a2697f2bc8a683add90f6c
Andrei <andrei_1002@hotmail.com>
2021-09-28 12:26:21 +0200

    feat: navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar.jsx'
 import './App.css';
+import { Switch } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div>
+        <Switch>
+          
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/MusicInTown (2).gif";
+import menuIcon from "../../assets/images/MenuIcon.png";
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="Navbar container-fluid">
+      <div className="navbar-flex">
+        <Link className="" href="/">
+          <img src={logo} alt="logo" className="logo" />
+        </Link>
+        <div className="" id="">
+          <form className="">
+            <input
+              className="search-form"
+              type="search"
+              placeholder="Search for artists or events..."
+            ></input>
+          </form>
+        </div>
+        <div>
+          <button className='btn'>Sign In </button>
+          <button className='btn'>Log In</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+
+//Menu para ver el perfil una vez logado.
+<Link className="" href="/">
+  <img src={menuIcon} alt="logo" className="logo" />
+</Link>;
