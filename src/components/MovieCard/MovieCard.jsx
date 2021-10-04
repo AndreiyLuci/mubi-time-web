35c565862849ad828b5db2bfdd9f663074963ed5
Andrei <andrei_1002@hotmail.com>
2021-10-04 19:15:26 +0200

    feat: MovieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,15 @@
-
+import {Link} from 'react-router-dom';
 
 export default function MovieCard() {
   return(
-    <div>
-
+    <div className="ProductCard">
+      <Link to={`/products/${id}`}>
+        <img className="ProductCard__image" src={images[0]} alt="" />
+        <div className="ProductCard__info">
+          <p>{name}</p>
+          <p>{price}€</p>
+        </div>
+      </Link>
     </div>
   )
 };
